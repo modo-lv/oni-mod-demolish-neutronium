8e8d1a4e606791c4ddcd244d114c05022cbe9938
modo_lv <modo@modo.lv>
2023-08-08 21:36:47 +0300

    Added Logging wrapper for prefixing messages.

---
+++ Main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DemolishNeutronium {
+  internal static partial class Main {
+    /// <summary>
+    /// Name of the mod for use in logging et al.
+    /// </summary>
+    public const String ModName = "Demolish Neutronium";
+  }
+}
---
+++ Main/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace
+namespace DemolishNeutronium {
+  internal static partial class Main {
+    /// <summary>
+    /// Wrapper for logging; automatically prefixes all messages with mod's name.
+    /// </summary>
+    public static class Log {
+      public static void Info(String line) {
+        Debug.Log($"[{ModName}] {line}");
+      }
+    }
+  }
+}
