afc105f59d40e6b74f384f2b0e458969d89ad428
modo_lv <modo@modo.lv>
2023-08-08 21:36:33 +0300

    Extension method for finding Neutronium Dust in an element list.

--- Extensions/ElementExtensions.cs
+++ Extensions/ElementExtensions.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 
 namespace DemolishNeutronium.Extensions {
   public static class ElementExtensions {
     public static Boolean IsNeutronium(this Element e) =>
       e.id == SimHashes.Unobtanium;
+
+    /// <summary>
+    /// Find Neutronium Dust (added by Rocketry Expanded mod) in a list of elements.
+    /// </summary>
+    /// <returns>Neutronium Dust element, or <c>null</c> if it is not in the list.</returns>
+    [CanBeNull]
+    public static Element FindNeutroniumDust([NotNull] this IEnumerable<Element> elements) {
+      return elements.First(e => e.id.ToString() == "UnobtaniumDust");
+    }
   }
 }
